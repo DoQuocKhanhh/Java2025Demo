5ceb064568ce071e6f8fada6f9ce35f4beda08f3
DoQuocKhanhh <127915117+DoQuocKhanhh@users.noreply.github.com>
2025-04-12 10:54:48 +0700

    commit note

--- BT1.java
+++ BT1.java
@@ -1,19 +1,13 @@
-import java.util.Scanner;
-
 public class BT1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Nhập số thứ nhất: ");
-        int a = scanner.nextInt();
-
-        System.out.print("Nhập số thứ hai: ");
-        int b = scanner.nextInt();
+        int a = 10; // bạn có thể thay bằng bất kỳ số nào
+        int b = 3;
 
         // Tính toán
         System.out.println("Tổng: " + (a + b));
         System.out.println("Hiệu: " + (a - b));
         System.out.println("Tích: " + (a * b));
+
         if (b != 0) {
             System.out.println("Thương: " + ((double) a / b));
             System.out.println("Chia lấy dư: " + (a % b));
--- BT2.java
+++ BT2.java
@@ -1,16 +1,11 @@
-import java.util.Scanner;
-
 public class BT2 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Nhập một số nguyên: ");
-        int number = scanner.nextInt();
+        int number = 7; // bạn có thể thay bằng số bất kỳ
 
         if (number % 2 == 0) {
-            System.out.println("Số " + number + " là số chẵn.");
+            System.out.println(number + " là số chẵn.");
         } else {
-            System.out.println("Số " + number + " là số lẻ.");
+            System.out.println(number + " là số lẻ.");
         }
     }
 }
